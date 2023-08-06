0a877c329c316936baff07808476c75bfbdd3ea1
n8tedawg <135954853+n8tedawg@users.noreply.github.com>
2023-08-07 10:38:46 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -30,6 +30,8 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
  let hatY = 90
  let sparkleX = 150
  let sparkleY = 50
+ let insidetriangleX = 90
+ let insidetriangleY = 110
 
  //line(100,0,100,200) //testlines
  //line(0,100,200,100)
@@ -54,15 +56,25 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
  strokeWeight(0);
  ellipse(ballX+15,ballY+5,60,60);
 
- beginShape(); //shine
+ fill(152, 14, 176); //magical triangle
+ triangle(insidetriangleX+4,insidetriangleY,insidetriangleX+50,insidetriangleY+5,insidetriangleX+25,insidetriangleY+49)
+
+ fill(252,252,252) // the white shine on the ball
+ beginShape();
  vertex (shineX+17,shineY-35);//95,60 original 
  bezierVertex(shineX,shineY-30,shineX-15,shineY-10,shineX-25,shineY);
  bezierVertex(shineX-30,shineY,shineX-15,shineY-50,shineX+17,shineY-35); //70,40
  endShape();
 
- textSize(60); //the number 8
+ //textSize(60); //TEXT the number 8
+ //fill(0,0,0)
+ //text("8",ballX,ballY+26);
+
+ textSize(13); //TEXT try again
  fill(0,0,0)
- text("8",ballX,ballY+26);
+ text("try",insidetriangleX+19,insidetriangleY+13)
+ textSize(10);
+ text("again",insidetriangleX+14,insidetriangleY+23)
 
  fill(248, 255, 51)
  quad(sparkleX,sparkleY,sparkleX+5,sparkleY+10,sparkleX,sparkleY+20,sparkleX-5,sparkleY+10) //top shine
